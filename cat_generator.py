d589703652e11e9b7383e1669cc76dfce6138d74
coz268 <125989787+coz268@users.noreply.github.com>
2023-09-15 11:37:43 +0300

    Update cat_generator.py

--- cat_generator.py
+++ cat_generator.py
@@ -3,5 +3,6 @@ import random
 PREFIXES = ["Мур", "Барс", "Вась", "Рыж", "Матр"]
 SUFFIXES = ["зик", "ик", "ька", "ок", "ун"]
 
+
 cat_name = random.choice(PREFIXES) + random.choice(SUFFIXES)
 print(cat_name)
